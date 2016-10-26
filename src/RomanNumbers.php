5b983569cb38139eb3db3386b943ca6fbe015017
Marc <mcalafellsmx@gmail.com>
2016-10-26 20:00:54 +0200

    Solving Problems

--- src/RomanNumbers.php
+++ src/RomanNumbers.php
@@ -1,25 +1,33 @@
 <?php
 
-class RomanNumbers{
+class RomanNumbers
+{
     protected $code = [
-        1 => 'I',
-        4 => 'IV',
-        5 => 'V',
+        1000 => 'M',
+        900 => 'CM',
+        500 => 'D',
+        400 => 'CD',
+        100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
+        10 => 'X',
         9 => 'IX',
-        10 => 'X'
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I',
     ];
 
 
-    public function convert($number){
-        $solution ='I';
-
-        if($number==6) {
-            $solution = $this->code[$number];
-            $number=0;
+    public function convert($number)
+    {
+        $solution = "";
+        foreach ($this->code as $decimal => $roman) {
+            if ($number >= $decimal) {
+                $solution .= $roman;
+                $number -= $decimal;
+            }
         }
-
-        $solution= $solution . str_repeat("I", $number);
-
+        return $solution;
     }
-
 }
